07cf8522b449684a58342237b6db1e365719762f
Art_n_Harsh <145191947+artnharsh@users.noreply.github.com>
2026-03-07 18:51:21 +0530

    Delete frontend directory

--- frontend/src/pages/ResultPage.tsx
+++
@@ -1,7 +0,0 @@
-export default function ResultPage() {
-  return (
-    <div className="h-screen flex items-center justify-center">
-      <h1 className="text-3xl font-bold">Game Result</h1>
-    </div>
-  );
-}
